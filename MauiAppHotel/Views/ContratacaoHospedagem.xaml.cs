f612274d6a486dafc30bf8ae601e72bdde320ec3
Elias B. Altas <Elias B. Altas@DESKTOP-V8SSBVJ>
2025-06-01 15:16:09 -0300

    add

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,13 +2,40 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+	App PropriedadesApp;
+
 	public ContratacaoHospedagem()
 	{
-		InitializeComponent();
+        InitializeComponent();
+
+		PropriedadesApp = (App)Application.Current;
+		pck_quarto.ItemsSource = PropriedadesApp.Lista_Quartos;
+		dtpck_checkin.MinimumDate = DateTime.Now;
+		dtpck_checkin.MaximumDate =new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+		dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+		dtpck_checkout.MaximumDate =dtpck_checkin.Date.AddMonths(6);
 	}
 	private void Button_Clicked(Object sender, EventArgs e)
 	{
 		Navigation.PushAsync(new Sobre());
+		try
+		{
+            Navigation.PushAsync(new HospedagemContratada());
+
+
+        }
+		catch (Exception ex)
+		{
+
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
 	}
 
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+DatePicker elemento = sender as DatePicker;
+		DateTime data_selecionada_checkin = elemento.Date;
+		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1); 
+		dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
+    }
 }
